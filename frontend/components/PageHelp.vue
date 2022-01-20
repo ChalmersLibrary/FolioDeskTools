ee20bb257bc040a4bd3f0dcda0d90bec0b445b07
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-01-20 14:26:42 +0100

    Update PageHelp.vue

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -10,6 +10,6 @@ Hämta och checka in som vanligt.<br><br>
     <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.</li>
     <li>Sätt Missing själv.</li>
     </ul>
-<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Missingrutin</a>
+<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Manual för missing</a>
   </div>
 </template>
