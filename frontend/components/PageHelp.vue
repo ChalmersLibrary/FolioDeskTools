cba5dd2750cb6081fc461f61a1c161c3e97f5170
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-01-20 11:11:10 +0100

    Update PageHelp.vue

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <b>Saknas boken på hyllan?</b>
-    1. Gör så här
-    2. ...
+    <ul>
+    <li>Om annat exemplar finns - flytta reservationen.</li>
+    <li>HB & Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.</li>
+    <li>Sätt Missing själv.</li>
+    </ul>
+<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Missingrutin HB & Kuggen</a>
   </div>
 </template>
