5a88d99a9e7a13e1aab3a1ca03eb07bc30db3418
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-01-20 12:26:36 +0100

    Update PageHelp.vue

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -2,7 +2,7 @@
   <div>
   <br><br>
   <b>Status In process</b><br>
-  HB: Finns i FjärrIn-rummet.<br>
+  HB & Kuggen: Finns i FjärrIn-rummet.<br>
 Hämta och checka in som vanligt.<br><br>
     <b>Saknas boken på hyllan?</b><br>
     <ul>
@@ -10,6 +10,6 @@ Hämta och checka in som vanligt.<br><br>
     <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.</li>
     <li>Sätt Missing själv.</li>
     </ul>
-<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Missingrutin HB &amp; Kuggen</a>
+<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Missingrutin</a>
   </div>
 </template>
