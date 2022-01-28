838e2ce09deb241be119acc6ef98aa5e640acb96
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-01-28 15:55:51 +0100

    Update PageHelp.vue

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -10,9 +10,9 @@
     <b>Om boken saknas på hyllan</b><br>
     <ol>
     <li>Om annat exemplar finns - flytta reservationen.</li>
-    <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.<br>
+    <li>HB &amp; Kuggen: Maila media.lib@chalmers.se och klistra in infon på denna lista. Gör inget med reservationen.<br>
     ACE: Tag bort reservationen och maila låntagaren.</li>
-    <li>Sätt Missing själv.</li>
+    <li>Sätt Missing direkt. Då slipper den dyka upp på nästa plocklista.</li>
     </ol>
 <a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Manual för missing</a>
   </div>
