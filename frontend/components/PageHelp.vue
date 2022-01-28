2fc9dbbad50660172668452169bf7db90f170ff0
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-01-28 15:51:41 +0100

    Update PageHelp.vue

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -2,18 +2,18 @@
   <div>
   <br><br>
   <b>Status In process</b><br>
-   <ul>
-  <li>HB &amp; Kuggen: Finns i FjärrIn-rummet på HB eller arbetas med av förvärvare/katalogisatör.<br>
+   <ol>
+  <li>HB &amp; Kuggen: Hämta i FjärrIn-rummet på HB. (Kan även arbetas med av förvärvare/katalogisatör.)<br>
   ACE: Finns på katalogiseringshyllan.</li>
-  <li>Hämta och checka in som vanligt.</li>
- </ul>
+  <li>Checka in som vanligt.</li>
+ </ol>
     <b>Saknas boken på hyllan?</b><br>
-    <ul>
+    <ol>
     <li>Om annat exemplar finns - flytta reservationen.</li>
     <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.<br>
     ACE: Tag bort reservationen och maila låntagaren.</li>
     <li>Sätt Missing själv.</li>
-    </ul>
+    </ol>
 <a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Manual för missing</a>
   </div>
 </template>
