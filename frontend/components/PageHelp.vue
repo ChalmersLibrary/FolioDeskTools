7c25604cf33c9f96e23af45b0d9f04341125b3ba
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-01-28 14:49:03 +0100

    Update PageHelp.vue

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -8,6 +8,7 @@ Hämta och checka in som vanligt.<br><br>
     <ul>
     <li>Om annat exemplar finns - flytta reservationen.</li>
     <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.</li>
+    <li>ACE: Tag bort reservationen och maila låntagaren.</li>
     <li>Sätt Missing själv.</li>
     </ul>
 <a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Manual för missing</a>
