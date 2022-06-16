f2ae86e67eef5d2042798d502b17bc0c8402b0ad
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-06-16 13:47:08 +0200

    Update MainInfo.vue

--- frontend/components/MainInfo.vue
+++ frontend/components/MainInfo.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <h2>Du måste vara på Chalmers nät</h2>
-    <p>Det fungerar även på VPN, men inte Always on VPN.</p>
-    <h2>Skapa app på dator - Microsoft Edge och Chrome</h2>
+    <h2>Välkommen kollegor på Chalmers bibliotek!</h2>
+    <p>Här kan du rensa reservationshyllan, skapa framplockningslistor och byta PIN-kod.</p>
+    <p>Du måste vara på Chalmers nätverk. Diskappen fungerar även på VPN, men inte Always on VPN.</p>
+    <h3>Lägg diskappen som en app på din dator - Microsoft Edge och Chrome</h3>
     <p>I URL-fältet finns en ikon. Klicka på den så installeras appen och du kan lägga den i verktygsfältet på datorn.</p>
     <p>Edge-ikonen visas nedan, i Chrome liknar den en dator med pil nedåt.</p>
     <img src="/install-web-app-icon-edge.png">
-    <h2>Tips för att använda på mobilen</h2>
-    <p>Lägg länken på startsidan / hemskärmen.</p>
-    <p>Använd gärna liggande skärm.</p>
-    <h3>iPhone</h3>
-    <p>Klicka på dela länk i verktygsfältet och sedan "Lägg till på hemskärmen"</p>
+    <h3>Att använda diskappen på mobilen</h3>
+    <p>Lägg gärna länken på startskärmen.</p>
+    <p>Liggande skärm fungerar bäst.</p>
+    <p>Iphone: Klicka på dela länk i verktygsfältet och sedan "Lägg till på hemskärmen"</p>
   </div>
 </template>
