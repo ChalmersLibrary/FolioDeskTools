3365f6e0eea22b44e3a0fcf3647e266009242d4e
MarieWi <52502488+MarieWi@users.noreply.github.com>
2022-08-08 13:39:21 +0200

    Update ShelfHelp.vue

--- frontend/components/ShelfHelp.vue
+++ frontend/components/ShelfHelp.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
   <br><br>
-  <b>BUGG! LISTAN STÄMMER INTE!</b></br>
-  <b>Kolla istället datum på väntehyllan manuellt.</b></br>
-  <br><br>
   <b>Saknas boken på reservationshyllan? </b><br>
     <ul>
     <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.</li>
-    <li>Sätt därefter Missing själv.</li>
+    <li>Sätt därefter itemet som Missing.</li>
     </ul>
 <a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Mer info om missing</a>
   </div>
