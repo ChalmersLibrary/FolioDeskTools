a6c483bbb45a4424c13802c642a0bab6ec75b180
larkov <lari.kovanen@chalmers.se>
2024-02-06 17:17:15 +0100

    Change link for more information.

--- frontend/components/PageHelp.vue
+++ frontend/components/PageHelp.vue
@@ -17,6 +17,6 @@
     ACE: Ta bort reservationen och maila låntagaren.</li>
     <li>Sätt därefter exemplaret som Missing.</li>
     </ol>
-<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Mer info om missing</a>
+<a href="https://chalmers.sharepoint.com/:w:/r/sites/ChalmersBibliotekssystemmedia/Shared%20Documents/01%20Dokument/03%20Manualer%20Infodisk%20och%20e-support/%C3%96vriga%20manualer%20och%20l%C3%A4nkar/Missingrutin%20Infodisk.docx?d=w98ebaee5c8cc485b93a01ca06a46bb5a&csf=1&web=1&e=IrK8he">Mer info om missing</a>
   </div>
 </template>
--- frontend/components/ShelfHelp.vue
+++ frontend/components/ShelfHelp.vue
@@ -6,6 +6,6 @@
     <li>HB &amp; Kuggen: Maila media.lib@chalmers.se. Kopiera infon på denna lista.</li>
     <li>Sätt därefter itemet som Missing.</li>
     </ul>
-<a href="https://chalmersuniversity.app.box.com/s/x6fjirc33ly9tykltqah0eo1xf2oove2/file/710097023701">Mer info om missing</a>
+<a href="https://chalmers.sharepoint.com/:w:/r/sites/ChalmersBibliotekssystemmedia/Shared%20Documents/01%20Dokument/03%20Manualer%20Infodisk%20och%20e-support/%C3%96vriga%20manualer%20och%20l%C3%A4nkar/Missingrutin%20Infodisk.docx?d=w98ebaee5c8cc485b93a01ca06a46bb5a&csf=1&web=1&e=IrK8he">Mer info om missing</a>
   </div>
 </template>
